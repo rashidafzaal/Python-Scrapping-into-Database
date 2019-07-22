356404ec49b78216912a79da0329bcd25b4e2bda
Rashid Afzaal <rashid.afzaal17@gmail.com>
2019-07-22 13:01:23 +0500

    Test successfull - saving into database
    Test successfull - saving into database

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Scrapping-into-Database
+This repository includes basic code of how to scrap data from a webpage and save into database.
--- scrap_and_save_into_database.py
+++ scrap_and_save_into_database.py
@@ -1,8 +1,35 @@
-#================= SQL Connection =================
+
+#=========================== import librarires ===========================
+import requests
+import MySQLdb
+from bs4 import BeautifulSoup
+
+#=========================== SQL Connection ===========================
 HOST = "localhost"
 USERNAME = "scraping_user"
 PASSWORD = "1234"
 DATABASE = "scraping_arbisoft"
 
-#================= URL to Scrap =================
+#=========================== URL to Scrap ===========================
 url_to_scrap = 'https://news.ycombinator.com/jobs'
+
+#=========================== Get HTML and Parse ===========================
+plain_html_text = requests.get(url_to_scrap)
+soup = BeautifulSoup(plain_html_text.text, "html.parser")
+
+#print(soup.prettify())
+
+basic_data_table = soup.find("table", {"class": "itemlist"});
+basic_data_cells = basic_data_table.findAll('td')
+
+
+#=========================== Saving into Database ===========================
+
+db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+cursor = db.cursor()
+sql = "INSERT INTO test(test_name) VALUES ('{}')".format("hello")
+try:
+    cursor.execute(sql)
+    db.commit()
+except:
+    db.rollback()
