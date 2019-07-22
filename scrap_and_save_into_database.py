c2bccc7a1765f8e83fdf842d103894e48ef9e048
Rashid Afzaal <rashid.afzaal17@gmail.com>
2019-07-22 12:06:51 +0500

    sql and url
    sql and url

---
+++ scrap_and_save_into_database.py
@@ -0,0 +1,8 @@
+#================= SQL Connection =================
+HOST = "localhost"
+USERNAME = "scraping_user"
+PASSWORD = "1234"
+DATABASE = "scraping_arbisoft"
+
+#================= URL to Scrap =================
+url_to_scrap = 'https://news.ycombinator.com/jobs'
