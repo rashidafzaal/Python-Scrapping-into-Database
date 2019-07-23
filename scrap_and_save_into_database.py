58c40ada0640da213c90ba73409c9661e77016a3
Rashid Afzaal <rashid.afzaal17@gmail.com>
2019-07-23 14:01:35 +0500

    task 1 done
    task 1 done

--- scrap_and_save_into_database.py
+++ scrap_and_save_into_database.py
@@ -1,4 +1,4 @@
-
+#Task 1
 #=========================== import librarires ===========================
 import requests
 import MySQLdb
@@ -17,19 +17,25 @@ url_to_scrap = 'https://news.ycombinator.com/jobs'
 plain_html_text = requests.get(url_to_scrap)
 soup = BeautifulSoup(plain_html_text.text, "html.parser")
 
-#print(soup.prettify())
-
 basic_data_table = soup.find("table", {"class": "itemlist"});
-basic_data_cells = basic_data_table.findAll('td')
+basic_data_cells = basic_data_table.findAll('a')
+my_filtered_array = []
 
-
+for i in range(3,len(soup.findAll('a'))-20):
+	print("\n")
+	indexVal = basic_data_cells[i].text.strip()
+	if indexVal.find(" ago") == -1 and indexVal.find(".com") == -1 and indexVal.find(".co") == -1 and indexVal.find(".io") == -1  and indexVal.find(".ai") == -1 and indexVal.find("-") == -1 and indexVal.find("/") == -1 and indexVal.find("–") == -1 and indexVal.find("More") == -1:
+		my_filtered_array.append(str(basic_data_cells[i].text.strip()))
+	else:
+		print("")
 #=========================== Saving into Database ===========================
 
+print(my_filtered_array)
 db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
 cursor = db.cursor()
-sql = "INSERT INTO test(test_name) VALUES ('{}')".format("hello")
+sql = "INSERT INTO arbisoft (title) VALUES (%s)"
 try:
-    cursor.execute(sql)
+    cursor.executemany(sql, my_filtered_array)
     db.commit()
 except:
     db.rollback()
